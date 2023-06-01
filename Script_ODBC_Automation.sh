d7055abbb8a4d87e062dacbe5e1c0cf320f7e350
ChokoBG <80669836+ChokoBG@users.noreply.github.com>
2023-06-01 16:58:53 +0300

    Add file
    Beta version of the Script

---
+++ Script_ODBC_Automation.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# List of servers
+servers=("192.168.100.4")
+
+# File path on the servers
+file_path="/tmp/text.txt"
+
+# Password for User
+password="1234Vitosha"
+
+# User to be used
+user="admin"
+
+# Full Function of script
+function esb_script { 
+    server=$1
+# Backup part of the script
+    read -p "Do you wish to proceed with creating backup for the file (y/n): " bkp
+    if [[ $bkp == y ]]; then
+        sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "cp \"$file_path\" \"/tmp/file_$(date +"%m%d%y").bkp\""
+        if [ $? -eq 0 ]; then
+            echo "Backup was successfully created on $server."
+        else
+            echo "Failed to make backup on $server."
+        fi
+    else
+        exit
+    fi  
+# Importing part of the script
+       read -p "Do you want to proceed with importing (y/n): " proceed
+        if [[ $import == y ]]; then
+                # Source server details
+                source_user="admin"
+                source_host="192.168.100.3"
+                source_file="/tmp/source.txt"
+
+                # Destination server details
+                destination_user="admin"
+                destination_host="192.168.10.4"
+                destination_file="/tmp/text.txt"
+
+                # Password variable
+                passwords="1234Vitosha"
+
+                # Read the content from the source file on the source server
+                source_content=$(sshpass -p "$passwords" ssh "${source_user}@${source_host}" "cat ${source_file}")
+
+                # Append the content to the destination file on the destination server
+                sshpass -p "$passwords" ssh "${destination_user}@${destination_host}" "echo '${source_content}' >> ${destination_file}"
+
+                if [ $? -eq 0 ]; then
+                        echo "Text was successfully imported on $server."
+                    else
+                        echo "Failed to import text on $server."
+                    fi
+        else
+            echo "Finished without importing the text on $server."
+        fi
+
+            #if [[ $proceed == y ]]; then 
+                #sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "import='cat /tmp/text.txt' && echo \"$import\" >> \"$file_path\""
+                  #  if [ $? -eq 0 ]; then
+                   #     echo "Text was successfully imported on $server."
+                   # else
+                    #    echo "Failed to import text on $server."
+                   # fi
+            #else   
+            #    echo "Finished without importing the text on $server."
+           # fi
+
+# Here are entered the variables needed for the execution of the commands
+echo ""
+echo ""
+read -p "Please, enter node name: " node
+    echo""
+    read -p "Please, enter what will be the name of the new resource name: " esbname
+        echo ""
+        read -p "Please, enter the name of the user: " user
+            echo ""
+            read -p "Please, enter the password for the user: "$'\n' -s dbpass
+echo ""
+echo ""
+# Running the commands
+    read -p "Do you want to proceed with 'mqsisetdbparms' command (y/n): " command
+        if [[ $command == y ]]; then
+            sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "mqsisetdbparms $node -n $esbname -u "$user"-p '$dbpass'"
+
+                echo "Do you want to proceed with 'mqsicvp' command (y/n) ?"
+                read command
+                    if [[ $command == y ]]; then
+                        sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "mqsicvp $node -p $esbname"
+                    else
+                        exit
+                    fi
+        else
+            exit
+        fi
+# Stopping the service
+    read -p "Do you want to proceed with stoping the service on server $server (y/n): " stop
+
+    if [[ $stop == y ]]; then
+        sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "mqsistop $node"
+    else
+        exit
+    fi
+# Starting the service 
+    read -p "Do you want to proceed with start the service on server $server (y/n): " start
+
+    if [[ $start == y ]]; then
+        sshpass -p $password ssh -o StrictHostKeyChecking=no "$user@$server" "mqsistart $node"
+    else
+        exit
+    fi
+}
+# This is the loop for the function
+for server in "${servers[@]}"; do
+    esb_script "$server"
+done
